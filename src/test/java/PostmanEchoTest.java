fcbc431fd16573c6a98fd572dd8e07f3986ed2c6
Anastasiia Kosterkina <nastya.kosterkina@mail.ru>
2023-12-29 13:21:40 +0300

    initial commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,28 @@
+package ru.netology;
+
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnSomeData() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
